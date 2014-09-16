0e115c2dd979a948ab3ebeacaba0d78cc09dcbba
beem4n <pipkin.zhora@gmail.com>
2014-09-16 22:00:18 +0700

    first push

--- js/main.js
+++ js/main.js
@@ -6,9 +6,15 @@ angular.module('notTetris', [])
         $scope.array100 = getArray(100);
         $scope.backBlocks = [];
         $scope.array100.forEach(setRandomColor);
+        $scope.greenCount = 0;
+        $scope.redCount = 0;
+        $scope.blueCount = 0;
         function setRandomColor(item, i, arr) {
             var randomColor = $scope.colorsArray[Math.floor(Math.random()*$scope.colorsArray.length)];
             $scope.backBlocks.push({id: i, color: randomColor, active: false});
+            if ($scope.backBlocks.color == 'green') {$scope.greenCount += 1;}
+            if ($scope.backBlocks.color == 'blue') {$scope.blueCount += 1;}
+            if ($scope.backBlocks.color == 'red') {$scope.redCount += 1;}
         }
         $scope.dump = $scope.backBlocks.count;
     }]);
